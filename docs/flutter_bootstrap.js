589bc8f00c9c76bd0db4c9c00d103347aafcea10
Balestrero Karim MTWIC <Karim.Balestrero@mt.com>
2026-08-21 12:28:19 +0200

    Update web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1103620764" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2094667372" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
